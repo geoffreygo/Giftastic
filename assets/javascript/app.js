089f2e45ad24d70089d575abb5038236611a6445
GGA <geoffrey.godwin-austen@outlook.com>
2018-08-12 23:28:30 -0600

    final code without comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var topics = [{movie:"The Matrix", presses: 0,}, {movie: "The Terminator", presses: 0,}, {movie: "Avengers", presses: 0,}, {movie: "Super Troopers", presses: 0,}];
+    var topics = [{movie:"Thor Ragnarok", presses: 0, favorite: false,}, {movie: "The Terminator", presses: 0, favorite: false,}, {movie: "Avengers", presses: 0, favorite: false,}, {movie: "Dawn of the Dead", presses: 0, favorite: false,}];
 
     function renderGifs(a) {
 
@@ -25,13 +25,13 @@ $(document).ready(function () {
             console.log(response);
             rspArray = response.data;
             for (var i = 0; i < rspArray.length; i++) {
-                var catDiv = $("<div class='card mr-3 mb-3 pb-2 float-left'></div>");
-                var cdBody = $("<div class='card-body'></div>")
-                // var gfTitle = $("<p>" + rspArray[i].title + "</p>");
-                var gfRating = $("<strong><p>Rating: " + rspArray[i].rating + "</p></strong>")
+                var catDiv = $("<div class='card mr-3 mb-3 pb-2 float-left gifCard'></div>");
+                var cdBody = $("<div class='card-body'></div>");
+                var gfRating = $("<strong><p class='text-center'>Rating: " + rspArray[i].rating + "</p></strong>");
+                var fav = $("<strong><p class='text-center pb-0' id='fav-tag'>Favorite</p></strong>");
                 var gfImg = $("<img class='card-img-top makeMeGo' src='" + rspArray[i].images.fixed_height_still.url + "' alt='gif'/>");
                 gfImg.attr({"data-still": rspArray[i].images.fixed_height_still.url, "data-animate": rspArray[i].images.fixed_height.url, "data-state": "still"});
-                cdBody.append(gfRating);
+                cdBody.append(gfRating, fav);
                 catDiv.append(gfImg, cdBody);
 
                 $("#gif-view").prepend(catDiv);
@@ -47,7 +47,7 @@ $(document).ready(function () {
         for (var i = 0; i < topics.length; i++) {
 
             var a = $("<button>");
-            a.addClass("gif btn btn-success mr-3 mt-3 mb-3");
+            a.addClass("gif btn btn-primary mr-3 mt-3 mb-3");
             a.attr({"data-name": topics[i].movie, type: "button"});
             console.log(topics[i].movie);
             a.text(topics[i].movie);
@@ -59,10 +59,11 @@ $(document).ready(function () {
         event.preventDefault();
 
         var gif = $("#new-cat").val().trim();
-        var newBtn = {movie: gif, presses: 0};
+        var newBtn = {movie: gif, presses: 0, favorite: false,};
         console.log(topics);
 
         topics.push(newBtn);
+        $("#new-cat").val("");
 
         renderButtons();
         console.log(topics);
@@ -89,6 +90,11 @@ $(document).ready(function () {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
           }
+    })
+
+    $(document).on("click", ".gifCard > .card-body", function(event) {
+        console.log($(this));
+        $("#favGifs").append($(this).parent());
     })
 
     renderButtons();
