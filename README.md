0a1533fb510afd2ae23a3fa95f2906325519deab
GGA <geoffrey.godwin-austen@outlook.com>
2018-08-13 23:18:22 -0600

    final code, adding local storage and readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Giftastic
+
+This application allows you to explore gifs based on movies. There is a row of buttons at the top which, when clicked, will 
+retrieve 10 fresh gifs from Giphy related to the title on the button. Pressing the button again will get you 10 more new gifs. 
+
+New buttons can be added by typing in movie titles in the box on the right and clicking the Add Button button. You will see
+the new button at the end of the row, and clicking it will retrieve 10 wonderful gifs related to the movie you just entered.
+
+The gifs are still when they load, but clicking on the image will cause them to play. Clicking on them again will cause them
+to stop playing.
+
+You can select favorites and they will move to the favorites section on the right of the screen. These favorites will always be
+here for you as long as you use the same browser on the same computer, even if you shut down your browser and come back later.
+
+Once you tire of a favorite, you can click the Remove button and it will vanish!
+
+Have fun and be careful, this app will make you gifstatic!!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     // array with initial buttons
-    var topics = [{movie:"Thor Ragnarok", presses: 0,}, {movie: "The Terminator", presses: 0,}, {movie: "Avengers", presses: 0,}, {movie: "Dawn of the Dead", presses: 0,}];
+    var topics = [{movie:"Thor Ragnarok", presses: 0,}, {movie: "Fantasia", presses: 0,}, {movie: "Heavy Metal", presses: 0,}, {movie: "The Babadook", presses: 0,}];
+    // array to keep favorites in, to write to localStorage
+    var favorites = [];
     // Function to retrieve gifs from giphy api, create cards for each gif, and place them on the page. Receives "data-name" of button as argument
     function renderGifs(a) {
         // places "data-name" in variable
@@ -25,7 +27,8 @@ $(document).ready(function () {
             for (var i = 0; i < rspArray.length; i++) {
                 var catDiv = $("<div class='card mr-3 mb-3 pb-2 float-left gifCard'></div>");
                 var cdBody = $("<div class='card-body'></div>");
-                var gfRating = $("<strong><p class='text-center'>Rating: " + rspArray[i].rating + "</p></strong>");
+                var gfRating = $("<p class='text-center rating'><strong>Rating: " + rspArray[i].rating + "</strong></p>");
+                gfRating.attr("rating", rspArray[i].rating);
                 var fav = $("<strong><p class='text-center pb-0' id='fav-tag'>Favorite</p></strong>");
                 var gfImg = $("<img class='card-img-top makeMeGo' src='" + rspArray[i].images.fixed_height_still.url + "' alt='gif'/>");
                 gfImg.attr({"data-still": rspArray[i].images.fixed_height_still.url, "data-animate": rspArray[i].images.fixed_height.url, "data-state": "still"});
@@ -50,6 +53,33 @@ $(document).ready(function () {
             a.text(topics[i].movie);
             $("#buttons-view").append(a);
         }
+    }
+    // function to read array of gif objects from local storage and put them up in the Favorites section of the page
+    function renderFavorites() {
+        // get the array from localStorage and parse it back into objects
+        var favs = JSON.parse(localStorage.getItem("favoriteGifs"));
+        // check to make sure it's an array, to avoid blowing up the browser
+        if (Array.isArray(favs)) {
+            // since its an array, we can put the objects back in the global array variable
+            favorites = favs;
+            // clear the div that holds the favorites before recreating them all
+            $("#favGifs").empty();
+            // This for loop reconstructs the Bootstrap card elements for the favorite gifs and prepends them to the Favorites div
+            for (var i = 0; i < favs.length; i++) {
+                var catDiv = $("<div class='card mr-3 mb-3 pb-2 float-left gifCard'></div>");
+                var cdBody = $("<div class='card-body'></div>");
+                var gfRating = $("<p class='text-center rating'><strong>Rating: " + favs[i].rating + "</strong></p>");
+                gfRating.attr("rating", favs[i].rating);
+                var fav = $("<strong><p class='text-center pb-0' id='del-tag'>Remove</p></strong>");
+                var gfImg = $("<img class='card-img-top makeMeGo' src='" + favs[i].stillUrl + "' alt='gif'/>");
+                gfImg.attr({ "data-still": favs[i].stillUrl, "data-animate": favs[i].animateUrl, "data-state": "still" });
+                cdBody.append(gfRating, fav);
+                catDiv.append(gfImg, cdBody);
+              
+                $("#favGifs").prepend(catDiv);
+            }
+        }
+
     }
     // click handler for button used to add new movie button
     $("#gif-search").on("click", function (event) {
@@ -88,12 +118,45 @@ $(document).ready(function () {
             $(this).attr("data-state", "still");
           }
     })
-    // click handler to move a "favorite" to the favorites section
-    $(document).on("click", ".gifCard > .card-body", function(event) {
-        console.log($(this));
+    // click handler to move a "favorite" to the favorites section and push it to the favorites array and localStorage
+    // had to specify the child item of .card-body so that clicking on the img would still cause it to animate
+    $("#gif-view").on("click", ".gifCard > .card-body", function(event) {
         $("#favGifs").append($(this).parent());
+        // set a variable to hold the .makeMeGo element, which is the img
+        var card = $(this).parent().children(".makeMeGo");
+        // set a variable to hold the .rating element, which is the rating
+        var rating = $(this).children(".rating");
+        // add the still url, animate url, and rating as an object to the favorites array. These are the 3 pieces
+        // of information that we need to rebuild the gif cards
+        favorites.push({"stillUrl":card.attr("data-still"), "animateUrl":card.attr("data-animate"), "rating": rating.attr("rating")});
+        // clear local storage
+        localStorage.clear();
+        // stringify our favorites array and put it in local storage
+        localStorage.setItem("favoriteGifs", JSON.stringify(favorites));
+
+        renderFavorites();
     })
-    // initial call to renderButtons
+    // click event handler for removing favorites
+    $("#favGifs").on("click", ".card-body", function(event) {
+        // set variable equal to the img element so that we can retrieve a url to compare
+        var urls = $(this).parent().children(".makeMeGo");
+        // remove the gif card from the Favorites section
+        $(this).parent().remove();
+        // Loop to check which object in the favorites array matches the card that was clicked to remove
+        for (var i = 0; i < favorites.length; i++) {
+            // when the match is found, remove the object from the array
+            if (urls.attr("data-still") == favorites[i].stillUrl) {
+                favorites.splice(i, 1);
+            }
+        }
+        // it is now gone from html and from the favorites array, but now we must remove it from localStorage
+        localStorage.clear();
+        localStorage.setItem("favoriteGifs", JSON.stringify(favorites));
+    })
+
+
+    // initial calls to renderButtons and renderFavorites
     renderButtons();
+    renderFavorites();
 
 })
